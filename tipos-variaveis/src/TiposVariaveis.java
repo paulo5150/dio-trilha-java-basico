7e203e4ce6cab691e346fd9bdaf813fca9bb30cf
Paulo Lima <paulo5150@live.com>
2023-07-19 23:20:38 -0300

    Final do bloco "tipos e variáveis"

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,30 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        // estude a classe String que representa texto na aplicação
+
+        String meuNome = "Paulo Roberto de Andrade Lima";
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;/*cuidado ao atribuir 
+                                                valores de variaveis sem 
+                                                observar a sua capacidade
+                                                e característica*/
+ 
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; /* "final" no inicio da declaração
+                                         de variável, indica Constante para
+                                          a liguagem JAVA, o nome em 
+                                          CAIXA_ALTA é convenção. */
+         
+
     }
 }
